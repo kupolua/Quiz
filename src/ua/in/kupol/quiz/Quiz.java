0d43429e5ae67721beace18353b2d48285634a5a
Pavel Kulakovsky <1215@i.ua>
2014-06-19 16:52:53 +0300

    Created stkotok brunch

--- src/ua/in/kupol/quiz/Quiz.java
+++ src/ua/in/kupol/quiz/Quiz.java
@@ -5,6 +5,7 @@ import ua.in.kupol.quiz.logic.QuizCreator;
 /**
  * Created by pavelkulakovsky on 16.06.14.
  * Fixed 8 ToDo
+ *
  */
 public class Quiz {
     public static void main(String[] args) {
