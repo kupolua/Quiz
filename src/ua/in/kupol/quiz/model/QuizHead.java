e15b2f2cb10d0ecf9674fa7b829c2005421c6c05
Pavel Kulakovsky <1215@i.ua>
2014-06-20 16:47:12 +0300

    14.	в QuizQuestion все поля public -  нарушение инкапсуляции. fixed: remove codehaus.jackson ADD google.GSON fixed: incapsulated field in QuizQuestion and QuizHead classes
    ... к тому же, нельзя смешивать представление (System.out) с моделью (QuizQuestion)
    fixed: it was move to class QuizReporter(but not completed)

--- src/ua/in/kupol/quiz/model/QuizHead.java
+++ src/ua/in/kupol/quiz/model/QuizHead.java
@@ -4,6 +4,7 @@ package ua.in.kupol.quiz.model;
  * Created by pavelkulakovsky on 17.06.14.
  */
 public class QuizHead {
+
     public String title;
     public String description;
     public QuizTypeLevel level;
